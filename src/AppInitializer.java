7ed10e96b4f20c9b1f4631fa76f1d880a8f7d02e
pramodsaranga <pramodsaranga31@gmail.com>
2022-09-23 16:56:59 +0530

    Updated Context

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -15,7 +15,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        URL resource = getClass().getResource("view/DriverShowForm.fxml");
+        URL resource = getClass().getResource("view/DashBoardForm.fxml");
         Parent load = FXMLLoader.load(resource);
         Scene scene = new Scene(load);
         primaryStage.setScene(scene);
--- src/controller/DashBoardFormController.java
+++ src/controller/DashBoardFormController.java
@@ -1,8 +1,25 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashBoardFormController {
     public AnchorPane DashBoardFormContext;
 
+    public void openLoginFormOnAction(ActionEvent actionEvent) throws IOException {
+        Stage window = (Stage) DashBoardFormContext.getScene().getWindow();
+        window.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+
+    }
+
+    public void openHomeFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openAboutFormOnAction(ActionEvent actionEvent) {
+    }
 }
